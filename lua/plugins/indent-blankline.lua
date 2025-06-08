4c1ed0b371090c2269c174acb07ddc5e7a47103d
Dan Olson <danolson100@gmail.com>
2025-06-08 10:57:33 -0500

    Update indent-blankline.lua so that it works correctly.

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,18 +1,24 @@
 -- Indent Guides
 return {
-    { 'lukas-reineke/indent-blankline.nvim',
-        --opts = {
-        --    char = '|',
-        --    show_trailing_blankline_indent = false,
-        --},
-        main = "ibl",
-	---@module "ibl"
-	---@type ibl.config
-       temp = function()
-           require("ibl").setup({})
+    'lukas-reineke/indent-blankline.nvim',
+    enabled = true,
+    main = "ibl",
+    opts = {
+        --indent = { char = "|" },
+        debounce = 50,
+        scope = { enabled = false},
+        exclude = { 
+            filetypes = { 'help',     'packer', 'nvimtree', 'dashboard', 'neo-tree' },
+            buftypes  = { 'terminal', 'nofile', 'quickfix' },
+        },
+    },
 
-            -- Toggle IndentGuides
-            vim.keymap.set( "n", "<leader>ig", "<Cmd>IndentGuidesToggle<CR>")
-       end,
-    }
+-- Setup Indent Guides
+    config = function(_, opts)
+        require("ibl").setup( opts)
+
+        -- Toggle IndentGuides
+        vim.keymap.set( "n", "<leader>ig", "<Cmd>IBLToggle<CR>")
+    end,
 }
+
